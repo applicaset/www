2ef902acbc8b931b234de234c2d8ed0e285250bc
Naser Mirzaei <nasermirzaei89@gmail.com>
2022-10-03 01:30:30 +0200

    Init

---
+++ nuxt.config.js
@@ -0,0 +1,42 @@
+export default {
+  head: {
+    title: 'ApplicaSet'
+  },
+  buildModules: [
+    '@nuxt/typescript-build',
+    [
+      '@nuxtjs/vuetify',
+      {
+        theme: {
+          themes: {
+            light: {
+              primary: '#00A693',
+              // secondary: '#424242',
+              // accent: '#82B1FF',
+              error: '#CC3333',
+              info: '#0067A5',
+              // success: '#4CAF50',
+              warning: '#DEB645'
+            },
+            dark: {
+              primary: '#00A693',
+              // secondary: '#424242',
+              // accent: '#FF4081',
+              error: '#CC3333',
+              info: '#0067A5',
+              // success: '#4CAF50',
+              warning: '#DEB645'
+            }
+          }
+        }
+      }
+    ]
+  ],
+  typescript: {
+    typeCheck: {
+      eslint: {
+        files: './**/*.{ts,js,vue}'
+      }
+    }
+  }
+}
---
+++ pages/index.vue
@@ -0,0 +1,5 @@
+<template>
+  <v-app>
+    <h1>Hello Future!</h1>
+  </v-app>
+</template>
