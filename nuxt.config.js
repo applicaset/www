16d173af964aee796bce0a2e985e5b5da5fa98f5
Naser Mirzaei <nasermirzaei89@users.noreply.github.com>
2022-12-28 00:26:19 +0330

    Add html lang attribute
    Signed-off-by: Naser Mirzaei <nasermirzaei89@users.noreply.github.com>

--- nuxt.config.js
+++ nuxt.config.js
@@ -5,7 +5,10 @@ export default {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' }
-    ]
+    ],
+    htmlAttrs: {
+      lang: 'en'
+    }
   },
   components: true,
   buildModules: [
