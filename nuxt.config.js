8d50d7660040bc967b5004cc9bea5fb65ea644a1
Naser Mirzaei <nasermirzaei89@users.noreply.github.com>
2022-12-28 00:18:59 +0330

    Add viewport meta
    Signed-off-by: Naser Mirzaei <nasermirzaei89@users.noreply.github.com>

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,7 +1,11 @@
 export default {
   target: 'static',
   head: {
-    title: 'ApplicaSet'
+    title: 'ApplicaSet',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' }
+    ]
   },
   components: true,
   buildModules: [
