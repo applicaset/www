ae5a05f9cffa05b2ad0a71c9cec18dfdb6c8c954
Naser Mirzaei <nasermirzaei89@gmail.com>
2023-06-05 22:07:03 -0400

    Add favicon

--- nuxt.config.js
+++ nuxt.config.js
@@ -9,7 +9,31 @@ export default {
         hid: 'description',
         name: 'description',
         content: 'We help you migrate to any cloud'
-      }
+      },
+      { name: 'msapplication-TileColor', content: '#008b8b' },
+      { name: 'theme-color', content: '#ffffff' }
+    ],
+    link: [
+      {
+        rel: 'apple-touch-icon',
+        sizes: '180x180',
+        href: '/apple-touch-icon.png'
+      },
+      {
+        rel: 'icon',
+        type: 'image/png',
+        sizes: '32x32',
+        href: '/favicon-32x32.png'
+      },
+      {
+        rel: 'icon',
+        type: 'image/png',
+        sizes: '16x16',
+        href: '/favicon-16x16.png'
+      },
+      { rel: 'manifest', href: '/site.webmanifest' },
+      { rel: 'mask-icon', href: '/safari-pinned-tab.svg', color: '#5bbad5' },
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.png' }
     ],
     htmlAttrs: {
       lang: 'en'
