0fe48963c3b43aaf8ac1fa8307d2987beaf36b7d
Naser Mirzaei <nasermirzaei89@gmail.com>
2025-01-30 18:57:44 -0500

    Move website header to a component

--- app.vue
+++ app.vue
@@ -1,48 +1,6 @@
 <template>
   <div>
-    <header class="border-b border-teal-700/20 bg-white sticky top-0 z-50">
-      <nav class="container mx-auto p-2 flex justify-between items-center">
-        <a href="#" class="flex items-center">
-          <img src="~/assets/images/logo.svg" alt="ApplicaSet" width="48" height="48">
-          <h1 class="mx-2">ApplicaSet</h1>
-        </a>
-        <!--<ul class="flex-row gap-4 hidden sm:flex">-->
-        <ul class="flex-row gap-4 flex">
-          <li>
-            <a href="#services" class="p-2">
-              Services
-            </a>
-          </li>
-          <li v-if="false">
-            <a href="#paas" class="p-2">
-              PaaS
-            </a>
-          </li>
-          <li v-if="false">
-            <a href="#pricing" class="p-2">
-              Pricing
-            </a>
-          </li>
-          <li>
-            <a href="#contact" class="p-2">
-              Contact
-            </a>
-          </li>
-          <li>
-            <a href="#about" class="p-2">
-              About
-            </a>
-          </li>
-        </ul>
-        <ul v-if="false" class="flex flex-row gap-4">
-          <li>
-            <as-button href="https://console.applicaset.com" is-primary>
-              Console
-            </as-button>
-          </li>
-        </ul>
-      </nav>
-    </header>
+    <website-header />
     <main class="relative">
       <hero-section />
       <services-section id="services" />
@@ -56,7 +14,6 @@
   </div>
 </template>
 <script setup>
-import AsButton from '~/components/basic/as-button.vue'
 import HeroSection from '~/components/www/hero-section.vue'
 import ServicesSection from '~/components/www/services-section.vue'
 import CustomersSection from '~/components/www/customers-section.vue'
@@ -65,4 +22,5 @@ import AboutSection from '~/components/www/about-section.vue'
 import PassSection from '~/components/www/pass-section.vue'
 import PricingSection from '~/components/www/pricing-section.vue'
 import WebsiteFooter from '~/components/www/website-footer.vue'
+import WebsiteHeader from '~/components/www/website-header.vue'
 </script>
---
+++ components/www/website-header.vue
@@ -0,0 +1,37 @@
+<template>
+  <header class="border-b border-teal-700/20 bg-white sticky top-0 z-50">
+    <nav class="container mx-auto p-2 flex justify-between items-center">
+      <a href="#" class="flex items-center">
+        <img src="~/assets/images/logo.svg" alt="ApplicaSet" width="48" height="48">
+        <h1 class="mx-2">ApplicaSet</h1>
+      </a>
+      <ul class="flex-row gap-4 flex">
+        <li>
+          <a href="#services" class="p-2">
+            Services
+          </a>
+        </li>
+        <li v-if="false">
+          <a href="#paas" class="p-2">
+            PaaS
+          </a>
+        </li>
+        <li v-if="false">
+          <a href="#pricing" class="p-2">
+            Pricing
+          </a>
+        </li>
+        <li>
+          <a href="#contact" class="p-2">
+            Contact
+          </a>
+        </li>
+        <li>
+          <a href="#about" class="p-2">
+            About
+          </a>
+        </li>
+      </ul>
+    </nav>
+  </header>
+</template>
