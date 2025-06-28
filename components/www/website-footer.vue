9d711df61f5ec9984dccf824dcba13e36f7c60ab
Naser Mirzaei <nasermirzaei89@gmail.com>
2025-06-28 16:56:55 -0400

    Add cursor-pointer to social icons

--- components/www/website-footer.vue
+++ components/www/website-footer.vue
@@ -5,15 +5,15 @@
     </h2>
     <div class="container mx-auto p-8 md:flex md:items-center md:justify-between">
       <div class="flex space-x-4 md:order-2 justify-center md:justify-end">
-        <a href="https://github.com/applicaset" class="text-gray-400 hover:text-gray-500" target="_blank">
+        <a href="https://github.com/applicaset" class="cursor-pointer text-gray-400 hover:text-gray-500" target="_blank">
           <span class="sr-only">GitHub</span>
           <icon-mdi-github class="h-6 w-6" aria-hidden="true" />
         </a>
-        <a href="https://www.linkedin.com/company/applicaset/" class="text-gray-400 hover:text-gray-500" target="_blank">
+        <a href="https://www.linkedin.com/company/applicaset/" class="cursor-pointer text-gray-400 hover:text-gray-500" target="_blank">
           <span class="sr-only">LinkedIn</span>
           <icon-mdi-linkedin class="h-6 w-6" aria-hidden="true" />
         </a>
-        <a href="https://www.facebook.com/applicaset/" class="text-gray-400 hover:text-gray-500" target="_blank">
+        <a href="https://www.facebook.com/applicaset/" class="cursor-pointer text-gray-400 hover:text-gray-500" target="_blank">
           <span class="sr-only">Facebook</span>
           <icon-mdi-facebook class="h-6 w-6" aria-hidden="true" />
         </a>
